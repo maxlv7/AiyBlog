1fcf820b6ba3192003c2083ad3017fa850658c8c
maxlv7 <admin@maxlv.org>
2018-07-18 22:14:04 +0800

    update utils

--- AiyBlog/utils/__init__.py
+++ AiyBlog/utils/__init__.py
@@ -1,10 +1,11 @@
 import functools
-
+import hashlib
 from flask import current_app as app
 from flask import render_template,session,abort
-from werkzeug.security import generate_password_hash,check_password_hash
 
-from AiyBlog.models import aiyblog_users
+from AiyBlog import db
+from AiyBlog.models import aiyblog_users,aiyblog_options
+
 def init_errors(app):
     @app.errorhandler(404)
     def page_not_found(error):
@@ -23,6 +24,11 @@ def init_errors(app):
         return error.description,502
 
 
+def init_utils(app):
+    app.jinja_env.globals.update(get_config=get_config)
+    app.jinja_env.globals.update(set_config=set_config)
+
+
 def authed():
     #when the uid is not esist,the default value is False
     return bool(session.get("uid",False))
@@ -33,6 +39,24 @@ def is_admin():
     else:
         return False
 
+
+def set_config(key,value):
+    config = aiyblog_options.query.filter_by(name=key).first()
+    if config:
+        config.value = value
+    else:
+        config = aiyblog_options(name=key,value=value)
+        db.session.add(config)
+    db.session.commit()
+    return None
+
+
+def get_config(key):
+    config = aiyblog_options.query.filter_by(name=key).first()
+    if config:
+        return config.value
+    else:
+        return none
 
 def authed_only(f):
     """
@@ -63,12 +87,11 @@ def admins_only(f):
     return decorate_func
 
 def pwd2hash(password):
-    return generate_password_hash(password)
+    return hashlib.sha1(password).hexdigest()
 
-def check_hash(hash,password):
-    return check_password_hash(hash,password)
 
 
 if __name__ == '__main__':
 
-    print(checkpwd())
+    print(generate_password_hash("123"))
+    print(check_password_hash("pbkdf2:sha256:50000$714BNkeq$b548df6ffa698fc14f9bf6c3793cc05b3c1","123"))
