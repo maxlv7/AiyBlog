fa3da40cbd844aae3107eac46b06db1214067f2d
maxlv7 <admin@maxlv.org>
2018-08-01 20:56:08 +0800

    add  theme static files

--- AiyBlog/admin/write.py
+++ AiyBlog/admin/write.py
@@ -12,6 +12,8 @@ def post():
 
     categories = aiyblog_metas.query.filter_by(type="category").all()
     tags = aiyblog_metas.query.filter_by(type="tag").all()
+
+    #写文章
     if request.method == "POST":
 
         cgs = request.form.getlist("category[]")
@@ -34,12 +36,27 @@ def post():
 
         # 先拿到cid,然后再找到对应的mid关系
         cid = content.cid
-        for mid in cgs:
-            relationship = aiyblog_relationships(cid=cid,mid=mid)
+        #用户没有选择分类，那么为默认分类
+        if len(cgs)==0:
+            relationship = aiyblog_relationships(cid=cid, mid=2)
             db.session.add(relationship)
             db.session.commit()
+        else:
+            for mid in cgs:
+                relationship = aiyblog_relationships(cid=cid,mid=mid)
+                db.session.add(relationship)
+                db.session.commit()
 
         return redirect(url_for("AiyBlog.admin.write.post"))
+
+
+    #修改文章
+    if request.args.get("cid"):
+        cid = request.args.get("cid")
+        blog = aiyblog_contents.query.filter_by(cid=cid).first()
+        return render_template("admin/write/post.html", cat=categories,blog=blog)
+    else:
+        print(2)
 
     return render_template("admin/write/post.html",cat=categories)
 
@@ -50,13 +67,13 @@ def page():
         title = request.form["title"] or "未命名页面"
         slug = request.form["slug"]
         text = request.form["markdown"]
-        order = request.form["order"]
+        order = int(request.form["order"])
         status = request.form["visibility"]
         try:
             allowComments = request.form["allowComments"]
         except:
             allowComments = "off"
-        page = aiyblog_contents(title=title,slug=slug,text=text,type="page",status=status,\
+        page = aiyblog_contents(title=title,slug=slug,text=text,type="page",created=time.time(),status=status,\
                                 allowComment=allowComments,order=order,authorId=session["uid"]
                                 )
         db.session.add(page)
