e93771853addf92752e0589f1c90162f9d8fc610
maxlv7 <admin@maxlv.org>
2018-08-03 22:24:40 +0800

    complete category count and write

--- AiyBlog/admin/action.py
+++ AiyBlog/admin/action.py
@@ -3,7 +3,7 @@ import time
 from flask import Blueprint
 from flask import url_for,request,redirect,session
 
-from AiyBlog.models import aiyblog_contents,aiyblog_relationships
+from AiyBlog.models import aiyblog_contents,aiyblog_relationships,aiyblog_metas
 from AiyBlog import db
 
 action = Blueprint(__name__,"action",url_prefix='/admin/action')
@@ -38,12 +38,19 @@ def edit_post():
         #用户没有选择分类，那么为默认分类
         if len(cgs)==0:
             relationship = aiyblog_relationships(cid=cid, mid=2)
+            t = aiyblog_metas.query.filter_by(mid=2).first()
+            t.count += 1
             db.session.add(relationship)
+            db.session.add(t)
             db.session.commit()
         else:
             for mid in cgs:
                 relationship = aiyblog_relationships(cid=cid,mid=mid)
+                #增加count数
+                t = aiyblog_metas.query.filter_by(mid=mid).first()
+                t.count+=1
                 db.session.add(relationship)
+                db.session.add(t)
                 db.session.commit()
 
         return redirect(url_for("AiyBlog.admin.manage.posts"))
@@ -82,19 +89,28 @@ def modify_post():
         #删除原来所有的分类(解除分类关系)
         rea = aiyblog_relationships.query.filter_by(cid=cid).all()
         for x in rea:
+            t = aiyblog_metas.query.filter_by(mid=x.mid).first()
+            t.count-=1
+            db.session.add(t)
             db.session.delete(x)
-            db.session.commit()
+        db.session.commit()
 
         # 添加
         if len(cgs) == 0:
             relationship = aiyblog_relationships(cid=cid, mid=2)
+            t = aiyblog_metas.query.filter_by(mid=2).first()
+            t.count+=1
+            db.session.add(t)
             db.session.add(relationship)
             db.session.commit()
         else:
             for mid in cgs:
                 relationship = aiyblog_relationships(cid=cid, mid=mid)
+                t = aiyblog_metas.query.filter_by(mid=mid).first()
+                t.count+=1
                 db.session.add(relationship)
-                db.session.commit()
+                db.session.add(t)
+            db.session.commit()
 
         return redirect(url_for("AiyBlog.admin.manage.posts"))
 
@@ -118,3 +134,66 @@ def delete_post():
             db.session.close()
         return redirect(url_for("AiyBlog.admin.manage.posts"))
 
+
+@action.route('/add-category',methods=["POST"])
+def add_category():
+    if request.method == "POST":
+        name = request.form["name"]
+        slug = request.form["slug"]
+        description = request.form["description"]
+
+        t = aiyblog_metas(name=name,slug=slug,description=description,type="category")
+        db.session.add(t)
+        db.session.commit()
+
+        return redirect(url_for("AiyBlog.admin.manage.categories"))
+
+
+@action.route('/modify-category',methods=["POST"])
+def modify_category():
+    if request.method == "POST":
+        mid = request.form["mid"]
+        name = request.form["name"]
+        slug = request.form["slug"]
+        description = request.form["description"]
+
+        #修改
+        t = aiyblog_metas.query.filter_by(mid=int(mid)).first()
+        t.name = name
+        t.slug = slug
+        t.description = description
+
+        db.session.add(t)
+        db.session.commit()
+        db.session.close()
+
+        return redirect(url_for("AiyBlog.admin.manage.categories"))
+
+@action.route('/delete-category',methods=["POST"])
+def delete_category():
+    if request.method == "POST":
+        ct_list = request.form.getlist("mid[]")
+
+        # 先删除分类
+        for mid in ct_list:
+            t = aiyblog_metas.query.filter_by(mid=mid).first()
+            db.session.delete(t)
+
+        #删除分类，解除与文章的关联，删除该分类下所有的内容
+        print("t.count is %d"%(t.count))
+        if t.count != 0:
+            #解除与文章的关联
+            cid_list = list()
+            for mid in ct_list:
+                t = aiyblog_relationships.query.filter_by(mid=mid).first()
+                cid_list.append(t.cid)
+                db.session.delete(t)
+            #删除内容
+            for cid in cid_list:
+                t = aiyblog_contents.query.filter_by(cid=cid).first()
+                db.session.delete(t)
+
+        db.session.commit()
+        db.session.close()
+
+        return redirect(url_for("AiyBlog.admin.manage.categories"))
--- AiyBlog/admin/write.py
+++ AiyBlog/admin/write.py
@@ -4,7 +4,7 @@ from flask import Blueprint
 from flask import render_template,request,url_for,redirect,session
 from AiyBlog import db
 from AiyBlog.models import aiyblog_metas,aiyblog_contents,aiyblog_relationships
-
+from AiyBlog.utils import find_mid_by_cid
 write = Blueprint(__name__,"write",url_prefix="/admin/write")
 
 @write.route('/post',methods=["GET","POST"])
@@ -13,50 +13,11 @@ def post():
     categories = aiyblog_metas.query.filter_by(type="category").all()
     tags = aiyblog_metas.query.filter_by(type="tag").all()
 
-    #写文章
-    if request.method == "POST":
-
-        cgs = request.form.getlist("category[]")
-        title = request.form["title"] or "未命名文章"
-        text = request.form["markdown"]
-        status = request.form["visibility"]
-        raw_tags = request.form["tags"]
-
-        new_tags = raw_tags.replace('，',',').split(',')
-
-        try:
-            allowComments = request.form["allowComments"]
-        except:
-            allowComments = "off"
-        # some oper
-        content = aiyblog_contents(title=title,created=int(time.time()),text=text,type="post",\
-                                   status=status,allowComment=allowComments,authorId=session["uid"])
-        db.session.add(content)
-        db.session.commit()
-
-        # 先拿到cid,然后再找到对应的mid关系
-        cid = content.cid
-        #用户没有选择分类，那么为默认分类
-        if len(cgs)==0:
-            relationship = aiyblog_relationships(cid=cid, mid=2)
-            db.session.add(relationship)
-            db.session.commit()
-        else:
-            for mid in cgs:
-                relationship = aiyblog_relationships(cid=cid,mid=mid)
-                db.session.add(relationship)
-                db.session.commit()
-
-        return redirect(url_for("AiyBlog.admin.write.post"))
-
-
     #修改文章
     if request.args.get("cid"):
         cid = request.args.get("cid")
         blog = aiyblog_contents.query.filter_by(cid=cid).first()
         return render_template("admin/write/post.html", cat=categories,blog=blog)
-    else:
-        print(2)
 
     return render_template("admin/write/post.html",cat=categories)
 
@@ -78,6 +39,6 @@ def page():
                                 )
         db.session.add(page)
         db.session.commit()
-        return redirect(url_for("AiyBlog.admin.write.page"))
+        return redirect(url_for("AiyBlog.admin.manage.posts"))
 
     return render_template("admin/write/page.html")
